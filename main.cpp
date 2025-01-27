85624c83e0563c7994334e8be0ed178246b7f122
TsubokiHayato <146918548+TsubokiHayato@users.noreply.github.com>
2025-01-27 11:40:54 +0900

    save

--- main.cpp
+++ main.cpp
@@ -11,13 +11,14 @@ int main() {
     std::string copy = original; // コピー
     auto end_copy = std::chrono::high_resolution_clock::now();
 
+    // コピーにかかった時間を計算
+    auto duration_copy = std::chrono::duration_cast<std::chrono::microseconds>(end_copy - start_copy).count();
+
     // 移動の時間計測開始
     auto start_move = std::chrono::high_resolution_clock::now();
     std::string moved = std::move(original); // 移動
     auto end_move = std::chrono::high_resolution_clock::now();
 
-    // コピーにかかった時間を計算
-    auto duration_copy = std::chrono::duration_cast<std::chrono::microseconds>(end_copy - start_copy).count();
     // 移動にかかった時間を計算
     auto duration_move = std::chrono::duration_cast<std::chrono::microseconds>(end_move - start_move).count();
 
